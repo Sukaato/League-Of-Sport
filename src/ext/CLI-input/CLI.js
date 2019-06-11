1a2c5707a654547576b88e9082b149d34a4fc8f2
Unknown <sukaato.dev@gmail.com>
2019-06-11 22:46:43 +0200

    LOP : V3
    Ajout d'input custom , metadonnées

---
+++ src/ext/CLI-input/CLI.js
@@ -0,0 +1,27 @@
+function newHTML(nodeName) {
+    return document.createElement(nodeName);
+}
+
+var labels = document.querySelectorAll('[class*="CLI"]');
+
+labels.forEach(e => {
+    var span = newHTML("span"),
+        labelText = e.outerText;
+
+    span.classList.add("CLI-placeholder");
+    span.innerText = labelText;
+
+    var input = e.children[0];
+    if (e.classList.contains("CLI-basic-in") || e.classList.contains("CLI-basic-in-out")) {
+        input.placeholder = " ";
+    }
+
+    e.innerText = null;
+    e.append(input, span);
+
+    if (e.classList.contains("CLI-highligth")) {
+        var highligth = newHTML("span");
+        highligth.classList.add("CLI-highligth");
+        e.append(highligth);
+    }
+});
---
+++ src/js/selection.js
@@ -0,0 +1,11 @@
+// DIV GLOBAL
+    result = getEltById('result');
+
+// BOUTON DE SELECTION
+var selectObj = document.querySelectorAll('#objectif + label')[0];
+    selectKda = document.querySelectorAll('#kda + label')[0];
+    selectJng = document.querySelectorAll('#jungle + label')[0];
+
+selectObj.addEventListener('click', () => toggleView(getEltById('objectif')));
+selectKda.addEventListener('click', () => toggleView(getEltById('kda')));
+selectJng.addEventListener('click', () => toggleView(getEltById('jungle')));
